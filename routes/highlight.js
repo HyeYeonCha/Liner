ed6c6bb92e382e7c5156df1f07f94f0ab0015634
HyeYeonCha <gpdus335@naver.com>
2021-03-14 01:03:22 +0900

    Modify api method

--- routes/highlight.js
+++ routes/highlight.js
@@ -4,9 +4,9 @@ const highlightControllers = require('../controllers/highlight');
 const router = express.Router();
 
 router.post('/create', highlightControllers.create);
-router.post('/update/highlight', highlightControllers.updateHighlight);
-router.post('/update/theme', highlightControllers.updateTheme);
-router.post('/delete', highlightControllers.delete);
+router.patch('/update/highlight', highlightControllers.updateHighlight);
+router.patch('/update/theme', highlightControllers.updateTheme);
+router.delete('/delete', highlightControllers.delete);
 router.post('/read/user', highlightControllers.readUser);
 router.post('/read/page', highlightControllers.readPage);
 
